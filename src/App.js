5d4846b063e79bf826ff120906e5ae8b4cb71a38
YevhenYevhen <yevhenvinnykov@gmail.com>
2021-11-10 12:37:32 +0100

    useSelector and useDispatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
-function App() {
+export const App = () => {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch() 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Counter: {counter} </div>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>INCREMENT</button>
+      <button onClick={() => dispatch({type:'DECREMENT'})}>DECREMENT</button>
     </div>
-  );
+  )
 }
 
 export default App;
